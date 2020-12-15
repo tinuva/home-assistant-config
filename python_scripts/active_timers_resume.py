7a35d58e4ca89d2c04acd5d1b4617d97d97e7fa7
David Bezuidenhout <tinuva@gmail.com>
2020-12-15 19:25:49 +0200

    add active timers resume script and automations

---
+++ python_scripts/active_timers_resume.py
@@ -0,0 +1,9 @@
+ts = hass.states.get('input_text.active_timers')
+if ts is not None and ts.state != 'unknown' and ts.state != '':
+  timers = ts.state.split(',')
+  for t in timers:
+    z = t.split()
+    duration = int(z[1]) - int(datetime.datetime.now().timestamp())
+    if duration > 0:
+      service_data = {'entity_id': 'timer.{}'.format(z[0]), 'duration':'{}'.format(duration)}
+      hass.services.call('timer', 'start', service_data, False)
