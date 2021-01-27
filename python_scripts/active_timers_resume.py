0ca618abd9db7b5e200883f738486e3516c06d37
David Bezuidenhout <tinuva@gmail.com>
2021-01-27 18:15:18 +0200

    add ability to restore paused timers after restart

--- python_scripts/active_timers_resume.py
+++ python_scripts/active_timers_resume.py
@@ -7,3 +7,15 @@ if ts is not None and ts.state != 'unknown' and ts.state != '':
     if duration > 0:
       service_data = {'entity_id': 'timer.{}'.format(z[0]), 'duration':'{}'.format(duration)}
       hass.services.call('timer', 'start', service_data, False)
+
+ts = hass.states.get('input_text.paused_timers')
+if ts is not None and ts.state != 'unknown' and ts.state != '':
+  timers = ts.state.split(',')
+  for t in timers:
+    z = t.split()
+    duration = int(z[1]) - int(datetime.datetime.now().timestamp())
+    if duration > 0:
+      service_data = {'entity_id': 'timer.{}'.format(z[0]), 'duration':'{}'.format(duration)}
+      hass.services.call('timer', 'start', service_data, False)
+      service_data = {'entity_id': 'timer.{}'.format(z[0])}
+      hass.services.call('timer', 'pause', service_data, False)
