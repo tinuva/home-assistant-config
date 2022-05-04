7f75f966b8607f37277cf1e318a6d4bae872fb43
David <tinuva@ipv6.za.org>
2022-05-04 11:51:52 +0200

    changes required to upgrade HA past ersion 2022.2.4

--- custom_components/goodwe/number.py
+++ custom_components/goodwe/number.py
@@ -9,9 +9,9 @@ from goodwe import Inverter, InverterError
 
 from homeassistant.components.number import NumberEntity, NumberEntityDescription
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import ENTITY_CATEGORY_CONFIG, PERCENTAGE, POWER_WATT
+from homeassistant.const import PERCENTAGE, POWER_WATT
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity import DeviceInfo
+from homeassistant.helpers.entity import DeviceInfo, EntityCategory
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .const import (
@@ -51,7 +51,7 @@ NUMBERS = (
         key="grid_export_limit",
         name="Grid export limit",
         icon="mdi:transmission-tower",
-        entity_category=ENTITY_CATEGORY_CONFIG,
+        entity_category=EntityCategory.CONFIG,
         unit_of_measurement=POWER_WATT,
         getter=lambda inv: inv.get_grid_export_limit(),
         setter=lambda inv, val: inv.inverter.set_grid_export_limit(val),
@@ -63,7 +63,7 @@ NUMBERS = (
         key="battery_discharge_depth",
         name="Depth of discharge (on-grid)",
         icon="mdi:battery-arrow-down",
-        entity_category=ENTITY_CATEGORY_CONFIG,
+        entity_category=EntityCategory.CONFIG,
         unit_of_measurement=PERCENTAGE,
         getter=lambda inv: inv.get_ongrid_battery_dod(),
         setter=lambda inv, val: inv.inverter.set_ongrid_battery_dod(val),
@@ -75,7 +75,7 @@ NUMBERS = (
         key="eco_mode_power",
         name="Eco mode power",
         icon="mdi:battery-charging-low",
-        entity_category=ENTITY_CATEGORY_CONFIG,
+        entity_category=EntityCategory.CONFIG,
         unit_of_measurement=PERCENTAGE,
         getter=lambda inv: inv.get_operation_mode(),
         setter=_set_eco_mode_power,
@@ -111,6 +111,12 @@ async def async_setup_entry(
         )
         if description.key == "eco_mode_power":
             domain_data[KEY_ECO_MODE_POWER] = entity
+
+        # DT family of inverter uses % instead of W in grid export limit
+        if type(inverter).__name__ == "DT" and description.key == "grid_export_limit":
+            description.unit_of_measurement = PERCENTAGE
+            description.step = 1
+            description.max_value = 100
 
         entities.append(entity)
 
