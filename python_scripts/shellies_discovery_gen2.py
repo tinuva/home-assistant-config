daa6648b4d428b1eea334b3dd894164162ba4e47
root <root@homeassistant.local.hass.io>
2025-01-12 08:09:42 +0200

    update python_scripts/ via HACS

--- python_scripts/shellies_discovery_gen2.py
+++ python_scripts/shellies_discovery_gen2.py
@@ -1,6 +1,6 @@
 """This script adds MQTT discovery support for Shellies Gen2 devices."""
 
-VERSION = "3.8.0"
+VERSION = "3.9.1"
 
 ATTR_BATTERY_POWERED = "battery_powered"
 ATTR_BINARY_SENSORS = "binary_sensors"
@@ -389,6 +389,9 @@ TPL_BLU_TRV_REPORT_EXTERNAL_TEMPERATURE = "{{{{{{^id^:1,^src^:^{source}^,^method
 TPL_BLU_TRV_SET_BOOST_TIME = "{{{{{{^id^:1,^src^:^{source}^,^method^:^BluTRV.Call^,^params^:{{^id^:{thermostat},^method^:^Trv.SetConfig^,^params^:{{^id^:0,^config^:{{^default_boost_duration^:value*60}}}}}}}}|to_json}}}}"
 TPL_BLU_TRV_SET_VALVE_POSITION = "{{{{{{^id^:1,^src^:^{source}^,^method^:^BluTRV.Call^,^params^:{{^id^:{thermostat},^method^:^Trv.SetPosition^,^params^:{{^id^:0,^pos^:value}}}}}}|to_json}}}}"
 TPL_BLU_TRV_VALVE_POSITION = "{{value_json.pos}}"
+TPL_BLU_TRV_CALIBRATION = (
+    "{{^ON^ if ^not_calibrated^ in value_json.get(^errors^,[]) else ^OFF^}}"
+)
 TPL_COUNTER = "{{value_json.counts.total}}"
 TPL_COUNTER_VALUE = "{{value_json.counts.xtotal}}"
 TPL_CLOUD = "{%if value_json.cloud.connected%}ON{%else%}OFF{%endif%}"
@@ -601,10 +604,11 @@ DESCRIPTION_SENSOR_BATTERY = {
     KEY_UNIT: UNIT_PERCENT,
     KEY_VALUE_TEMPLATE: TPL_BATTERY_PERCENT,
 }
-DESCRIPTION_BLU_TRV_VALVE_POSITION = {
+DESCRIPTION_SENSOR_BLU_TRV_VALVE_POSITION = {
     KEY_ENABLED_BY_DEFAULT: False,
     KEY_NAME: "Valve position",
     KEY_ICON: "mdi:pipe-valve",
+    KEY_ENTITY_CATEGORY: ENTITY_CATEGORY_DIAGNOSTIC,
     KEY_STATE_TOPIC: TOPIC_STATUS_BLU_TRV,
     KEY_UNIT: UNIT_PERCENT,
     KEY_VALUE_TEMPLATE: TPL_BLU_TRV_VALVE_POSITION,
@@ -877,6 +881,14 @@ DESCRIPTION_SENSOR_LAST_RESTART = {
     KEY_NAME: "Last restart",
     KEY_STATE_TOPIC: TOPIC_STATUS_RPC,
     KEY_VALUE_TEMPLATE: TPL_UPTIME,
+}
+DESCRIPTION_SENSOR_BLU_TRV_CALIBRATION = {
+    KEY_DEVICE_CLASS: DEVICE_CLASS_PROBLEM,
+    KEY_ENABLED_BY_DEFAULT: True,
+    KEY_ENTITY_CATEGORY: ENTITY_CATEGORY_DIAGNOSTIC,
+    KEY_NAME: "Calibration",
+    KEY_STATE_TOPIC: TOPIC_STATUS_BLU_TRV,
+    KEY_VALUE_TEMPLATE: TPL_BLU_TRV_CALIBRATION,
 }
 DESCRIPTION_SENSOR_OVERPOWER = {
     KEY_DEVICE_CLASS: DEVICE_CLASS_PROBLEM,
@@ -1583,10 +1595,13 @@ SUPPORTED_MODELS = {
     MODEL_BLU_TRV: {
         ATTR_NAME: "Shelly BLU TRV",
         ATTR_MODEL_ID: "SBTR-001AEU",
+        ATTR_BINARY_SENSORS: {
+            SENSOR_CALIBRATION: DESCRIPTION_SENSOR_BLU_TRV_CALIBRATION
+        },
         ATTR_SENSORS: {
             SENSOR_SIGNAL_STRENGTH: DESCRIPTION_SENSOR_BLU_TRV_SIGNAL_STRENGTH,
             SENSOR_BATTERY: DESCRIPTION_SENSOR_BLU_TRV_BATTERY,
-            SENSOR_VALVE_POSITION: DESCRIPTION_BLU_TRV_VALVE_POSITION,
+            SENSOR_VALVE_POSITION: DESCRIPTION_SENSOR_BLU_TRV_VALVE_POSITION,
         },
         ATTR_BUTTONS: {
             BUTTON_CALIBRATE: DESCRIPTION_BUTTON_BLU_TRV_CALIBRATE,
@@ -1933,16 +1948,20 @@ SUPPORTED_MODELS = {
         ATTR_MODEL_ID: "S3GW-1DBT001",
         ATTR_GEN: 3,
         ATTR_BINARY_SENSORS: {
-            SENSOR_CLOUD: DESCRIPTION_SLEEPING_SENSOR_CLOUD,
-            SENSOR_FIRMWARE: DESCRIPTION_SLEEPING_SENSOR_FIRMWARE,
-        },
-        ATTR_SENSORS: {
-            SENSOR_LAST_RESTART: DESCRIPTION_SLEEPING_SENSOR_LAST_RESTART,
-            SENSOR_SSID: DESCRIPTION_SLEEPING_SENSOR_SSID,
-            SENSOR_WIFI_IP: DESCRIPTION_SLEEPING_SENSOR_WIFI_IP,
-            SENSOR_WIFI_SIGNAL: DESCRIPTION_SLEEPING_SENSOR_WIFI_SIGNAL,
+            SENSOR_CLOUD: DESCRIPTION_SENSOR_CLOUD,
+            SENSOR_FIRMWARE: {},
         },
         ATTR_BUTTONS: {BUTTON_RESTART: DESCRIPTION_BUTTON_RESTART},
+        ATTR_SENSORS: {
+            SENSOR_LAST_RESTART: DESCRIPTION_SENSOR_LAST_RESTART,
+            SENSOR_SSID: DESCRIPTION_SENSOR_SSID,
+            SENSOR_WIFI_IP: DESCRIPTION_SENSOR_WIFI_IP,
+            SENSOR_WIFI_SIGNAL: DESCRIPTION_SENSOR_WIFI_SIGNAL,
+        },
+        ATTR_UPDATES: {
+            UPDATE_FIRMWARE: DESCRIPTION_UPDATE_FIRMWARE,
+            UPDATE_FIRMWARE_BETA: DESCRIPTION_UPDATE_FIRMWARE_BETA,
+        },
         ATTR_MIN_FIRMWARE_DATE: 20250109,
     },
     MODEL_HT_G3: {
@@ -3981,6 +4000,9 @@ def get_binary_sensor(
         topic = encode_config_topic(
             f"{disc_prefix}/binary_sensor/{device_id}-{sensor}/config"
         )
+
+    if not description:
+        return topic, ""
 
     if profile == ATTR_COVER:
         return topic, ""
